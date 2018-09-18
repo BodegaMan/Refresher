c28815ca72c85b96b42183df7c69ca67e252abf3
BodegaMan <wisebodega@gmail.com>
2018-09-18 14:18:46 -0500

    Ch01

--- SiSharp/Program.cs
+++ SiSharp/Program.cs
@@ -10,6 +10,31 @@ namespace SiSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Start ... ");
+            var person = Person.Parse("John");
+            person.Introduce("Mosh");
+
+            Console.ReadLine();
+
         }
     }
+
+    public class Person
+    {
+        public string Name;
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+
+        }
+    }
+
+
 }
